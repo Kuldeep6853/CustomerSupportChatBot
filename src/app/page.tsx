58cdb9cb9a5e0ecbc868e4ee21f72d4743f63015
Kuldeep singh <145250371+Kuldeep6853@users.noreply.github.com>
2026-02-25 01:50:10 +0530

    some update in the project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,12 @@ import HomeClient from "./components/HomeClient";
 
 
 export default async function Home() {
-  const session=await getSession()
-  const email=session?.user?.email;
+  const session = await getSession()
+  const email = session?.user?.email ?? ""
+
   return (
     <>
-    <HomeClient email={email} />
+      <HomeClient email={email} />
     </>
   );
 }
